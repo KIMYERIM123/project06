af9df665978732000a83cb0fce7110d685bf4490
KIMYERIM123 <133613841+KIMYERIM123@users.noreply.github.com>
2023-06-13 18:04:13 +0900

    Add files via upload

--- js/project06.js
+++ js/project06.js
@@ -4,4 +4,20 @@ $(function () {
     autoplaySpeed: 3000,
     arrows: false,
   });
+
+  $(".menu_slide").slick({
+    arrows: false,
+    dots: true,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+  });
+
+  $('.main_menu .menu_arrows .left').on('click', function () {
+    $('._slide').slick('slickPrev')
+  });
+  $('.main_menu .menu_arrows .right').on('click', function () {
+    $('.menu_slide').slick('slickNext')
+  });
+
 });
